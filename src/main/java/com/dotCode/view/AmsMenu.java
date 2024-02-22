dabcfa68b753b2b27e37fca559bcb5c98cf62429
limchanwool <cksdnf90@gmail.com>
2024-02-22 17:50:53 +0900

    initial commit
    # 5. 사원관리 분기 생성 (view.AmsMenu)

--- src/main/java/com/dotCode/view/AmsMenu.java
+++ src/main/java/com/dotCode/view/AmsMenu.java
@@ -26,11 +26,12 @@ public class AmsMenu {
 
                 System.out.println("============ AMS ============");
                 System.out.println("1. 출근");
+                System.out.println("2. 공지사항");
                 System.out.println("2. 나의 정보 열람");
                 System.out.println("3. 부재 신청");
                 System.out.println("4. 증빙서류 제출");
                 if ( registDAO.checkAdmin() == 0 ){
-                    System.out.println("5. 사원 정보 열람");
+                    System.out.println("5. 사원 관리");
                     System.out.println("6. 출결 현황 관리");
                     System.out.println("7. 서류 제출 현황");
                 }
@@ -65,6 +66,22 @@ public class AmsMenu {
                         case 4:
                             break;
                         case 5:
+                            System.out.println("============ AMS ============");
+                            System.out.println("1. 사원 등록");
+                            System.out.println("2. 사원 정보 조회");
+                            System.out.println("3. 사원 해고");
+                            System.out.println("=============================");
+                            sc.nextLine();
+                            int selectChoice = sc.nextInt();
+                            switch ( selectChoice ) {
+                                case 1:
+                                    break;
+                                case 2:
+                                    break;
+                                case 3:
+                                    break;
+
+                            }
                             break;
                         case 6:
                             break;
